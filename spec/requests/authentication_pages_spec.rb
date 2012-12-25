b56cf9c959bb9d57df578cea7cc97bd1b44366be
kevindigital84 <kevin.leugers@gmail.com>
2012-12-24 22:10:22 -0500

    still editing users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,6 +12,8 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
       before do
         fill_in "Email",    with: user.email
         fill_in "Password", with: user.password
@@ -22,6 +24,7 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
     end
   end
 end
